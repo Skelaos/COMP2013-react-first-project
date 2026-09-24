b9ba49c5624d9dc789f14a0bc43bbbcda7ed3fc3
Cole Herron <cbherron97@gmail.com>
2026-09-24 15:17:45 -0400

    Pre-final submission updated

--- src/Components/DiamondCard.tsx
+++ src/Components/DiamondCard.tsx
@@ -5,13 +5,18 @@ interface DiamondCardProps {
   sale?: boolean;
 }
 
-export default function DiamondCard(props: DiamondCardProps) {
+export default function DiamondCard({
+  image,
+  productName,
+  price,
+  sale,
+}: DiamondCardProps) {
   return (
     <div className="DiamondCard">
-      <img src={props.image} width="100px"></img>
-      <h2>{props.productName}</h2>
-      <p>{props.price}</p>
-      <p style={{ color: "red", fontWeight: "bold" }}>{props.sale && "SALE"}</p>
+      <img src={image} width="100px"></img>
+      <h2>{productName}</h2>
+      <p>{price}</p>
+      <p style={{ color: "red", fontWeight: "bold" }}>{sale && "SALE"}</p>
     </div>
   );
 }
